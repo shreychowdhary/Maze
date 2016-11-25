9004193b90005ecbc94bbced455f50d671199d25
Shrey <shrey.chowdhary1@gmail.com>
2016-11-24 19:32:40 -0500

    created graph

--- js/maze.js
+++ js/maze.js
@@ -19,7 +19,6 @@ var Cell = function(x,y,degrees) {
     this.degrees = degrees;
     this.visited = false;
     this.neighbors = [];
-    this.walls = [1,1,1,1];
 }
 
 var Graph = function(maxArc){
@@ -41,18 +40,91 @@ var Graph = function(maxArc){
             this.cells[y][x] = new Cell(x,y,degrees);
         }
     }
+
+    this.startCell.neighbors = [this.cells[1][0],null,null,null];
+    this.startCell.visited = true;
+    for(y = 1; y < this.cells.length; y++){
+        for(x = 0; x < this.cells[y].length; x++){
+            this.cells[y][x].neighbors.push(null,null,null);
+            if(y < this.cells.length-1 && this.cells[y+1].length > this.cells[y].length){
+                this.cells[y][x].neighbors.push(null,null);
+            }
+            else{
+                this.cells[y][x].neighbors.push(null);
+            }
+        }
+    }
+    this.exitCell = this.cells[17][0];
 }
 
 circleGraph = new Graph(10);
+
+function generateMaze(curCell,lastCell){
+    curCell.visited = true;
+    x = curCell.x;
+    y = curCell.y;
+    nearBy = [];
+    //goes left,down,right,(top) or (top right, top left)
+    nearBy.push(circleGraph.cells[y][mod(x-1,circleGraph.cells[y].length)]);
+    if(y == 1){
+        nearBy.push(circleGraph.cells[0][0]);
+    }
+    else if(circleGraph.cells[y] > circleGraph.cells[y-1]){
+        nearBy.push(circleGraph.cells[y-1][Math.floor(x/2)]);
+    }
+    else{
+        nearBy.push(circleGraph.cells[y-1][x]);
+    }
+    nearBy.push(circleGraph.cells[y][mod(x+1,circleGraph.cells[y].length)]);
+    if(y < circleGraph.cells.length-1){
+        if(circleGraph.cells[y+1] > circleGraph.cells[y]){
+            nearBy.push(circleGraph.cells[y+1][x*2]);
+            nearBy.push(circleGraph.cells[y+1][x*2+1]);
+        }
+        else{
+            nearBy.push(circleGraph.cells[y+1][x]);
+        }
+    }
+    visitable = [];
+    for(i = 0; i < nearBy.length; i++){
+        if(!nearBy[i].visited){
+            visitable.push(nearBy[i]);
+        }
+    }
+
+    if(visitable.length == 0){
+        return;
+    }
+
+    nextCell = visitable[Math.floor(Math.random() * visitable.length)];
+    curCell.neighbors[nearBy.indexOf(nextCell)] = nextCell;
+    curCell.neighbors[nearBy.indexOf(lastCell)] = lastCell;
+    console.log(curCell.neighbors);
+    generateMaze(nextCell,curCell);
+}
+generateMaze(circleGraph.cells[1][0],circleGraph.startCell);
+
 
 //create cells based on width
 //based on cell number you can draw lines around it
 //
 function drawGrid(){
+    context = canvas.getContext("2d");
+    context.lineWidth = 5;
+    context.lineCap = "round";
 
-
+    context.beginPath();
+    context.arc(radius,radius,radius*.01,0,2*Math.PI);
+    context.stroke();
+    context.fill();
+    for(i = 1; i < 4; i++){
+        context.beginPath();
+        context.arc(radius,radius,radius*.05,i*Math.PI/2,(i+1)*Math.PI/2);
+        context.stroke();
+    }
 }
 
+drawGrid();
 
 function drawCircle(){
     if(canvas.getContext){
@@ -85,4 +157,8 @@ function drawCircle(){
         context.fill();
     }
 }
-drawCircle();
+
+
+function mod(n, m) {
+        return ((n % m) + m) % m;
+}
